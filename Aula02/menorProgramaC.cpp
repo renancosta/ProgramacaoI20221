f7a241a31227459c4781266db6b6e921b948a5b0
Renan Costa Filgueiras <renan2727@hotmail.com>
2022-03-17 11:02:24 -0300

    Aula02

---
+++ Aula02/menorProgramaC.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+main(){
+	int idade,idade2;
+	float altura=1.8,peso;
+	idade = 85;
+	printf("Sua idade eh: %d",idade);
+	printf("Sua idade é %d \n E sua altura eh %f",idade,altura);
+}
